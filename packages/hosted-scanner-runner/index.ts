180f989ef128a02b495912cd7806c7eec1aa182a
Brian Le <188601252+LeDoNguyenTu@users.noreply.github.com>
2026-08-27 07:50:18 +0800

    feat: export hosted scanner runner [skip ci]

---
+++ packages/hosted-scanner-runner/index.ts
@@ -0,0 +1,8 @@
+export {
+  HOSTED_PHASE3_SCANNER_DESCRIPTORS,
+  HOSTED_PHASE3_SCANNER_PROFILE_ID,
+  HOSTED_PHASE3_SCANNER_PROFILE_VERSION,
+  HOSTED_PHASE3_TOOL_VERSION,
+  createHostedPhase3Scanners,
+} from "./profile";
+export { HostedScannerRunnerError, runHostedRepositoryScan } from "./run";
