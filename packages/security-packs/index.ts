30b9439ac431e8ccb738201ef9ab3c4a6674d26c
Brian Le <188601252+LeDoNguyenTu@users.noreply.github.com>
2026-09-04 08:05:42 +0800

    feat: run selected Security Packs deterministically

--- packages/security-packs/index.ts
+++ packages/security-packs/index.ts
@@ -3,4 +3,6 @@ export * from "./error";
 export * from "./finding";
 export * from "./literal-matcher";
 export * from "./path-pattern";
+export * from "./registry";
+export * from "./scanner";
 export { assertSecurityPackCompatibility, loadSecurityPackManifest } from "./parse";
