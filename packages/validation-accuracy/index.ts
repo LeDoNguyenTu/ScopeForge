90d0206437f97719898038a20907fbd8a9e46952
Brian Le <188601252+LeDoNguyenTu@users.noreply.github.com>
2026-09-05 22:43:51 +0800

    feat: export Phase 8A reporting APIs [skip ci]

--- packages/validation-accuracy/index.ts
+++ packages/validation-accuracy/index.ts
@@ -17,6 +17,10 @@ export {
   type ValidationRuleResult,
   type ValidationScannerFamily,
 } from "./contracts";
+export {
+  runValidationAccuracyCli,
+  type ValidationAccuracyCliOptions,
+} from "./cli";
 export { ValidationAccuracyError } from "./error";
 export {
   classifyValidationCase,
@@ -29,5 +33,7 @@ export {
   computeDerivedMetrics,
 } from "./metrics";
 export { loadValidationCorpus } from "./parse";
+export { serializeValidationAccuracyJson } from "./report-json";
+export { renderValidationAccuracyMarkdown } from "./report-markdown";
 export { readVerifiedValidationManifest } from "./safe-read";
 export { createValidationScanner } from "./scanners";
