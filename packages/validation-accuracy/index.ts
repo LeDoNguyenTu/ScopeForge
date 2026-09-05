afbffd9b66b424d08af6888340cdb149eda66fdb
Brian Le <188601252+LeDoNguyenTu@users.noreply.github.com>
2026-09-05 22:36:56 +0800

    feat: export Phase 8A metrics APIs [skip ci]

--- packages/validation-accuracy/index.ts
+++ packages/validation-accuracy/index.ts
@@ -3,12 +3,18 @@ export {
   type LoadedValidationCase,
   type LoadedValidationCorpus,
   type ValidationAccuracyErrorCode,
+  type ValidationAccuracyResult,
   type ValidationCaseLabel,
   type ValidationCaseOutcome,
   type ValidationCaseOutcomeKind,
   type ValidationCaseV1,
   type ValidationContractMismatch,
   type ValidationCorpusV1,
+  type ValidationCounts,
+  type ValidationCoverage,
+  type ValidationDerivedMetrics,
+  type ValidationProvenance,
+  type ValidationRuleResult,
   type ValidationScannerFamily,
 } from "./contracts";
 export { ValidationAccuracyError } from "./error";
@@ -16,7 +22,12 @@ export {
   classifyValidationCase,
   evaluateValidationCase,
   evaluateValidationCases,
+  evaluateValidationCorpus,
 } from "./evaluate";
+export {
+  aggregateValidationResult,
+  computeDerivedMetrics,
+} from "./metrics";
 export { loadValidationCorpus } from "./parse";
 export { readVerifiedValidationManifest } from "./safe-read";
 export { createValidationScanner } from "./scanners";
