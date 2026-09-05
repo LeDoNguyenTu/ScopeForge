5f22a7ae2856070159dd192c9426ef1f754bb5c7
Brian Le <188601252+LeDoNguyenTu@users.noreply.github.com>
2026-09-05 22:33:25 +0800

    feat: export Phase 8A evaluation APIs [skip ci]

--- packages/validation-accuracy/index.ts
+++ packages/validation-accuracy/index.ts
@@ -4,11 +4,19 @@ export {
   type LoadedValidationCorpus,
   type ValidationAccuracyErrorCode,
   type ValidationCaseLabel,
+  type ValidationCaseOutcome,
   type ValidationCaseOutcomeKind,
   type ValidationCaseV1,
+  type ValidationContractMismatch,
   type ValidationCorpusV1,
   type ValidationScannerFamily,
 } from "./contracts";
 export { ValidationAccuracyError } from "./error";
+export {
+  classifyValidationCase,
+  evaluateValidationCase,
+  evaluateValidationCases,
+} from "./evaluate";
 export { loadValidationCorpus } from "./parse";
 export { readVerifiedValidationManifest } from "./safe-read";
+export { createValidationScanner } from "./scanners";
