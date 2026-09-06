5495dba4652a7ecdf7558bc98b2bfbc47c68fca7
Brian Le <188601252+LeDoNguyenTu@users.noreply.github.com>
2026-09-07 03:25:01 +0800

    refactor: expose resolved Phase 8C evidence parser [skip ci]

--- packages/validation-publication/index.ts
+++ packages/validation-publication/index.ts
@@ -2,6 +2,6 @@ export * from "./cli";
 export * from "./contracts";
 export * from "./error";
 export * from "./normalize";
-export * from "./parse";
+export * from "./parse-publication";
 export * from "./report-json";
 export * from "./report-markdown";
