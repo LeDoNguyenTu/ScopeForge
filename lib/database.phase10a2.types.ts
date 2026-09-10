207d5f2eb1d33a1438f15c7fbcb4644e1206e667
Brian Le <188601252+LeDoNguyenTu@users.noreply.github.com>
2026-09-11 06:52:17 +0800

    feat: add Phase 10A2 database overlay [skip ci]

---
+++ lib/database.phase10a2.types.ts
@@ -0,0 +1,27 @@
+import type { Json } from "./database.types";
+import type { Phase10a1Database } from "./database.phase10a1.types";
+
+export type Phase10a2Functions = Phase10a1Database["public"]["Functions"] & {
+  register_private_repository_snapshot_worker_node: {
+    Args: {
+      target_credential_hash: string;
+      target_software_version: string;
+    };
+    Returns: Json;
+  };
+  enqueue_private_repository_snapshot_worker_task: {
+    Args: {
+      target_workspace_id: string;
+      target_asset_id: string;
+      target_actor_id: string;
+      target_github_repository_link_id: string;
+    };
+    Returns: Json;
+  };
+};
+
+export type Phase10a2Database = Omit<Phase10a1Database, "public"> & {
+  public: Omit<Phase10a1Database["public"], "Functions"> & {
+    Functions: Phase10a2Functions;
+  };
+};
